f48dac6c145a8b3257e77a4d97d6b5920363c3fd
Qingyao Ai <aiqy@cs.umass.edu>
2019-01-11 14:54:03 -0500

    Add gpu support and reduce evaluation frequency

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     ],
     install_requires=[
         'keras >= 2.0.5',
-        'tensorflow >= 1.1.0',
+        'tensorflow-gpu >= 1.1.0',
         'nltk >= 3.2.3',
         'numpy >= 1.12.1',
         'six >= 1.10.0',
